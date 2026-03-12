c8c37548f620648cef98765b911e3dae5e8cfe7c
Radivojevic Aleksandar <radivojevic.aleksandar91@gmail.com>
2026-03-12 02:16:25 +0100

    Moved path metadata into Component enum to eliminate chooseComponent() if-else

--- XMIParser/src/main/java/com/project/parser/Component.java
+++ XMIParser/src/main/java/com/project/parser/Component.java
@@ -1,12 +1,27 @@
 package com.project.parser;
 
 public enum Component {
-    ENTITY,
-	CONTROLLER,
-	REPOSITORY,
-	LIST,
-	FORM,
-	SERVICE,
-    DAO,
-    MAPPER
+
+    ENTITY     ("entities\\",    "",           ".java"),
+    CONTROLLER ("controller\\",  "Controller", ".java"),
+    REPOSITORY ("repository\\",  "Repository", ".java"),
+    SERVICE    ("service\\",     "Service",    ".java"),
+    DAO        ("dao\\",         "Dao",        ".java"),
+    MAPPER     ("mapper\\",      "Mapper",     ".java"),
+    LIST       ("",              "List",       ".html"),
+    FORM       ("",              "Form",       ".html");
+
+    private final String subfolder;
+    private final String suffix;
+    private final String extension;
+
+    Component(String subfolder, String suffix, String extension) {
+        this.subfolder = subfolder;
+        this.suffix = suffix;
+        this.extension = extension;
+    }
+
+    public String getSubfolder()  { return subfolder; }
+    public String getSuffix()     { return suffix; }
+    public String getExtension()  { return extension; }
 }
