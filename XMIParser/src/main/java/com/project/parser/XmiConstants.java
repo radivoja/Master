cb3f7e1e051856405d9ea05a0743f2e6fe018c89
Radivojevic Aleksandar <radivojevic.aleksandar91@gmail.com>
2026-03-12 02:15:08 +0100

    Moved XMI constants to XmiConstants class

---
+++ XMIParser/src/main/java/com/project/parser/XmiConstants.java
@@ -0,0 +1,11 @@
+package com.project.parser;
+
+public class XmiConstants {
+    static final String MY_META_MODEL_PROFILE = "MyMetaModel";
+    static String BASE_CLASS = "base_Class";
+    static String BASE_PROPERTY = "base_Property";
+    static String UML_CLASS = "uml:Class";
+    static String UML_PROPERTY = "uml:Property";
+    static String XMI_TYPE = "xmi:type";
+    static String XMI_ID = "xmi:id";
+}
