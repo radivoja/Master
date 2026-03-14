a32c7da777744517ce57c5fe55c97445a988de13
Radivojevic Aleksandar <radivojevic.aleksandar91@gmail.com>
2026-03-14 03:18:09 +0100

    Added INDEX to Component enum and expanded XmiConstants Added INDEX component entry to support index.html generation. Added STATIC, FREEMARKER_EXTENSION, INDEX, HTML, MODELS, MODEL constants.

--- XMIParser/src/main/java/com/project/parser/XmiConstants.java
+++ XMIParser/src/main/java/com/project/parser/XmiConstants.java
@@ -1,6 +1,6 @@
 package com.project.parser;
 
-public class XmiConstants {
+public final class XmiConstants {
     static final String MY_META_MODEL_PROFILE = "MyMetaModel";
     static String BASE_CLASS = "base_Class";
     static String BASE_PROPERTY = "base_Property";
@@ -8,4 +8,10 @@ public class XmiConstants {
     static String UML_PROPERTY = "uml:Property";
     static String XMI_TYPE = "xmi:type";
     static String XMI_ID = "xmi:id";
+    static String STATIC = "src/main/resources/static";
+    static String FREEMARKER_EXTENSION = ".ftlh";
+    static String INDEX = "index";
+    static String HTML = ".html";
+    static String MODELS = "models";
+    static String MODEL = "model";
 }
